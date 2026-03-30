cc328cc7423745e9aaf3517759bca394d7130305
unknown <souperdaffy@gmail.com>
2026-03-30 16:53:42 +0200

    Modif du script : Tableau + début de gestion des erreurs

--- Documents/Temp/Ajout_AD_interactif.ps1
+++ Documents/Temp/Ajout_AD_interactif.ps1
@@ -1,19 +1,40 @@
+# Ajouter une option de création en masse d'utilisateur à partir d'un fichier .csv
+
 #--------------------------------------------------------
 # CREATION D'UNE NOUVELLE UNITE ORGANISATIONNELLE AD :
 #--------------------------------------------------------
 
 #Récupération des valeurs :
+# Name
 $OU_name = Read-Host "Nom de l'OU "
-$OU_path = Read-Host "Destination sous la forme -> DC=nom_de_domaine,DC=TLD (Exemple de TLD: com ou fr) "
+$OU_name_exist = Get-ADObject -Filter "Name -eq '$OU_name'" -ErrorAction SilentlyContinue
+if($OU_name_exist){
+    Write-Host "Attention ! L'OU '$OU_name' existe déjà dans : " -ForegroundColor Yellow -NoNewline
+    Write-Host $OU_name_exist.DistinguishedName -ForegroundColor Cyan
+}
+# Path (bloc à vérifier)
+$OU_valid_path = $false
+while (-not $OU_valid_path) {
+    $OU_path = Read-Host "Destination sous la forme d'un DN -> DC=nom_de_domaine,DC=TLD (Exemples de TLD: com/fr/local) "
+    if(Get-ADObject -Identity $OU_path -ErrorAction SilentlyContinue){Write-Host "L'emplacement '$OU_path' est déjà utilisé..." -ForegroundColor Yellow}
+    else {$OU_valid_path = $true}
+}
+# Suppression accidentelle
 $Input_protected = Read-Host "Protéger contre la suppression accidentelle ? ([O]ui / [N]on)"
 $OU_protected = $true
 if($Input_protected -eq "N"){$OU_protected = $false}
 
+#Création du Tableau des paramètres :
+$OU_params = @{
+    Name = $OU_name
+    Path = $OU_path
+    ProtectedFromAccidentalDeletion = $OU_protected
+}
+
 #Création de l'OU :
-Write-Host "Création de l'OU..." -ForegroundColor Green
-New-ADOrganizationalUnit -Name "$OU_name" `
-    -Path "$OU_path" `
-    -ProtectedFromAccidentalDeletion $OU_protected
+Write-Host "Création de l'OU..." -ForegroundColor DarkYellow
+New-ADOrganizationalUnit @OU_params
+Write-Host "Création de l'OU terminée !" -ForegroundColor Green
 
 
 #-------------------------------------
@@ -21,26 +42,44 @@ New-ADOrganizationalUnit -Name "$OU_name" `
 #-------------------------------------
 
 #Récupération des valeurs :
-$Group_name = Read-Host "Nom du nouveau groupe "
-$Group_path = Read-Host "Destination sous la forme -> OU=nom_OU,DC=nom_de_domaine,DC=TLD "
-
+# Name
+$Group_name = Read-Host "Nom de l'OU "
+$Group_name_exist = Get-ADObject -Filter "Name -eq '$Group_name'" -ErrorAction SilentlyContinue
+if($Group_name_exist){
+    Write-Host "Attention ! Le groupe '$Group_name' existe déjà dans : " -ForegroundColor Yellow -NoNewline
+    Write-Host $Group_name_exist.DistinguishedName -ForegroundColor Cyan
+}
+# Path (bloc à vérifier)
+$Group_valid_path = $false
+while (-not $Group_valid_path) {
+    $Group_path = Read-Host "Destination sous la forme d'un DN -> OU=nom_OU,DC=nom_de_domaine,DC=TLD (Exemples de TLD: com/fr/local) "
+    if(Get-ADObject -Identity $Group_path -ErrorAction SilentlyContinue){Write-Host "L'emplacement '$Group_path' est déjà utilisé..." -ForegroundColor Yellow}
+    else {$OU_valid_path = $true}
+}
+# Type de Groupe
 $Input_type = Read-Host "Groupe de [S]écurité ou [D]istribution (par défaut S)? "
 $Group_type = "Security"
 if ($Input_type -eq "D"){$Group_type = "Distribution"}
 else {$Group_type = "Security"}
-
+# Étendue
 $Input_scope = Read-Host "Étendue [D]omainLocal, [G]lobal, [U]niversal (par défaut G) "
 $Group_scope = "Global"
 if($Input_scope -eq "D"){$Group_scope = "DomainLocal"}
 elseif($Input_scope -eq "U"){$Group_scope = "Universal"}
 else{$Group_scope = "Global"}
 
+#Création du Tableau des paramètres :
+$Group_params = @{
+    Name = $Group_name
+    Path = $Group_path
+    GroupCategory = $Group_type
+    GroupScope = $Group_scope
+}
+
 #Création du groupe :
-Write-Host "Création du groupe..." -ForegroundColor Green
-New-ADGroup -Name "$Group_name" `
-    -Path "$Group_path" `
-    -GroupCategory "$Group_type" `
-    -GroupScope $Group_scope
+Write-Host "Création du groupe..." -ForegroundColor DarkYellow
+New-ADGroup @Group_params
+Write-Host "Création du groupe terminée !" -ForegroundColor Green
 
 
 #-----------------------------------------
@@ -52,7 +91,7 @@ $User_givenname = Read-Host "Prénom "
 $User_surname = Read-Host "Nom de famille "
 $User_samaccountname = Read-Host "Identifiant de connexion (Ex : jdupont) "
 $User_principal_name = Read-Host "Identifiant de connexion (Ex : jdupont@entreprise.local) "
-$User_path = Read-Host "Destination sous la forme -> OU=nom_OU,DC=nom_de_domaine,DC=TLD "
+$User_path = Read-Host "Destination sous la forme d'un DN -> OU=nom_OU,DC=nom_de_domaine,DC=TLD (Exemples de TLD: com/fr/local) "
 $User_pass = Read-Host "Donner un mot de passe " -AsSecureString
 
 $Input_enable = Read-Host "Activer le compte ? ([O]ui / [N]on) "
@@ -67,15 +106,21 @@ $Input_pass_never_expire = Read-Host "Le mot de passe n'expire jamais ? ([O]ui /
 $User_pass_never_expire  = $false
 if ($Input_pass_never_expire -eq "O"){$User_pass_never_expire = $true}
 
+#Création du Tableau des paramètres :
+$User_params = @{
+    Name = "$User_givenname $User_surname"
+    GivenName = $User_givenname
+    Surname = $User_surname
+    SamAccountName = $User_samaccountname
+    UserPrincipalName = $User_principal_name
+    Path = $User_path
+    AccountPassword = $User_pass
+    Enabled = $User_enable
+    ChangePasswordAtLogon = $User_change_pass
+    PasswordNeverExpires = $User_pass_never_expire
+}
+
 #Création du user
-Write-Host "Création de l'utilisateur..." -ForegroundColor Green
-New-ADUser -Name "$User_givenname $User_surname" `
-    -GivenName $User_givenname `
-    -Surname $User_surname `
-    -SamAccountName $User_samaccountname `
-    -UserPrincipalName $User_principal_name `
-    -Path $User_path `
-    -AccountPassword $User_pass `
-    -Enabled $User_enable `
-    -ChangePasswordAtLogon $User_change_pass `
-    -PasswordNeverExpires $User_pass_never_expire
+Write-Host "Création de l'utilisateur..." -ForegroundColor DarkYellow
+New-ADUser @User_params
+Write-Host "Création de l'utilisateur terminée !" -ForegroundColor Green
